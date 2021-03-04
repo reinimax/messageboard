adbbac1407a28c27835934b159871998fc64580d
reinimax <reinhold.schachner@hotmail.com>
2021-03-04 11:31:16 +0100

    Add buttons for login and registration

--- views/inc/nav.php
+++ views/inc/nav.php
@@ -1,3 +1,10 @@
-<nav>
-Login
+<nav class="navbar navbar-expand-xl bg-light fixed-top justify-content-end">
+<ul class="navbar-nav mx-5">
+    <li class="nav-item">
+      <a class="nav-link btn btn-outline-info mx-2" href="/login">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link btn btn-outline-info mx-2" href="/register">Register</a>
+    </li>
+  </ul>
 </nav>
