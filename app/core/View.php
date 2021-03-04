9a8d91361241103f81916f5ce121a5293dca649f
reinimax <reinhold.schachner@hotmail.com>
2021-03-04 12:11:57 +0100

    Flesh out the render() function Call a new protected function getView() that returns a view that is then inserted into the template as {content}. Clarify in which form data is received by the render() function.

--- app/core/View.php
+++ app/core/View.php
@@ -4,11 +4,18 @@ namespace app\core;
 
 class View
 {
+    /**
+     * Renders a view
+     * @param $data Expects an associative array with keys for title and content,
+     * where content is the name of the view that is to be rendered. Data, for example
+     * a list of posts from the database, is accessed with the key data.
+     */
     public function render($data)
     {
         $template = $this->getTemplate();
-        $title = $data[0];
-        $content = $data[1];
+        $title = $data['title'] ?? 'Messageboard';
+        $receivedData = $data['data'] ?? '';
+        $content = $this->getView($data['content'], $receivedData);
         $output = str_replace(['{title}', '{content}'], [$title, $content], $template);
         echo $output;
     }
@@ -19,6 +26,13 @@ class View
         // simply using file_get_contents wouldn't work if the file contains any php
         ob_start();
         include ROOT.'/views/template.php';
+        return ob_get_clean();
+    }
+
+    protected function getView($page, $data)
+    {
+        ob_start();
+        include ROOT.'/views/'.$page;
         return ob_get_clean();
     }
 }
