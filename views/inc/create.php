ac5546256ee13f9126b8016ea5293f317c33cb0a
reinimax <reinhold.schachner@hotmail.com>
2021-03-05 16:19:23 +0100

    Add a link for creating new posts when logged in

---
+++ views/inc/create.php
@@ -0,0 +1,3 @@
+<div class="alert alert-info">
+  What do you have on your mind today? <a href="/create" class="alert-link">Write a new post</a> and share your thoughts with the world.
+</div>
--- views/index.php
+++ views/index.php
@@ -1,3 +1,8 @@
 <?php
 
+use app\lib\Session;
+
+if (Session::init()->checkLogin()) {
+    include ROOT.'/views/inc/create.php';
+}
 echo $data;
