8b65b16678c636275958f7b11cacb7583190f072
reinimax <reinhold.schachner@hotmail.com>
2021-03-05 15:49:47 +0100

    Create a proper navbar for the logged in user with logout button

--- views/inc/nav_user.php
+++ views/inc/nav_user.php
@@ -1,3 +1,11 @@
-<nav>
-Welcome, <?php echo $_SESSION['user']; ?>
+<nav class="navbar navbar-expand-xl bg-light fixed-top justify-content-between">
+  <a class="navbar-brand mx-5" href="/">Startpage</a>
+  <ul class="navbar-nav mx-5">
+    <span class="navbar-text">
+    Welcome, <strong><?php echo $_SESSION['user']; ?></strong>
+    </span>
+    <li class="nav-item">
+      <a class="nav-link btn btn-outline-info mx-2" href="/logout">Logout</a>
+    </li>
+  </ul>
 </nav>
