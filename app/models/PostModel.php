8d1245726de6f69a7da76e2d0f77247c2aac2203
reinimax <reinhold.schachner@hotmail.com>
2021-03-05 17:06:25 +0100

    Add a function to save a new post in the DB

--- app/models/PostModel.php
+++ app/models/PostModel.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use app\lib\MySql;
+use PDO;
+use PDOException;
 
 class PostModel
 {
@@ -22,5 +24,28 @@ class PostModel
         // test
         $test = 'I\'m a test!';
         return $test;
+    }
+
+    /**
+     * Saves a new post in the database
+     * @param array $data The validated data from the registration form
+     * @return array with the success- or error-message
+     */
+    public function save($data)
+    {
+        $savePost = <<<SQL
+            INSERT INTO posts (user_id, title, content) VALUES (:user, :title, :content);
+        SQL;
+
+        try {
+            $statement = $this->pdo->prepare($savePost);
+            $statement->bindParam(':user', $_SESSION['user_id'], PDO::PARAM_STR);
+            $statement->bindParam(':title', $data['title'], PDO::PARAM_STR);
+            $statement->bindParam(':content', $data['message'], PDO::PARAM_STR);
+            $statement->execute();
+        } catch (PDOException $e) {
+            return ['error' => $e->getMessage()];
+        }
+        return ['success' => 'You successfully posted a message'];
     }
 }
