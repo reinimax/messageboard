b2e3f06291025ee3ca15bce79b82d6424f728cd1
reinimax <reinhold.schachner@hotmail.com>
2021-03-06 17:37:38 +0100

    Add button for deleting current user's posts

--- views/index.php
+++ views/index.php
@@ -21,11 +21,21 @@ echo '<h1>The latest posts</h1>';
 
 foreach ($data['data'] as $item) {
     $date = (DateTime::createFromFormat('Y-m-d H:i:s', $item['updated_at']))->format('D, j M Y, H:i');
+    if ($item['user'] === $_SESSION['user']) {
+        $delete =  '<form action="/delete.php" method="POST">';
+        $delete .= '<input type="hidden" name="_method" value="delete">';
+        $delete .= '<button class="btn py-0 px-1" type="submit"><i class="fas fa-trash-alt"></i></button>';
+        $delete .= '</form>';
+    } else {
+        $delete = '';
+    }
+
+
     echo '<div class="card my-3">';
     echo '<div class="card-header">';
     echo '<div class="d-flex justify-content-between">';
     echo '<strong>'.$item['title'].'</strong>';
-    echo '<span>by '.$item['user'].'<span class="mx-2"></span>'.$date.'</span>';
+    echo '<span class="d-flex align-items-center">by '.$item['user'].'<span class="mx-2">'.$delete.'</span>'.$date.'</span>';
     echo '</div>';
     echo '</div>';
     // pre tags to enable newlines and multiple spaces ... format them later
