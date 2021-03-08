4af2956d12d851d4fd226c42cf9fe9a7f0ae95b5
reinimax <reinhold.schachner@hotmail.com>
2021-03-08 15:24:22 +0100

    Add searchbar

---
+++ views/inc/search.php
@@ -0,0 +1,10 @@
+<form class="form-inline" action="/search.php" method="GET">
+    <input class="form-control mr-2" type="text" name="search" placeholder="Search">
+    <label for="limit" class="mr-2"> for </label>
+    <select class="form-control mr-2" id="limit" name="limit">
+        <option value="all">All</option>
+        <option value="user">User</option>
+        <option value="title">Title</option>
+    </select>
+    <button class="btn btn-outline-primary" type="submit">Search</button>
+</form>
