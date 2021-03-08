f5f5bfec3f17f98149498988d65d45bf8a72bb70
reinimax <reinhold.schachner@hotmail.com>
2021-03-08 13:28:21 +0100

    Add edit button for logged in user

--- views/index.php
+++ views/index.php
@@ -22,11 +22,15 @@ echo '<h1>The latest posts</h1>';
 foreach ($data['data'] as $item) {
     $date = (DateTime::createFromFormat('Y-m-d H:i:s', $item['updated_at']))->format('D, j M Y, H:i');
     if ($item['user'] === $_SESSION['user']) {
+        // build edit button
+        $edit = '<a href="/edit.php?id='.$item['id'].'" class="btn py-0 px-1"><i class="fas fa-pencil-alt"></i></a>';
+        // build delete button
         $delete =  '<form action="/delete.php?id='.$item['id'].'" method="POST">';
         $delete .= '<input type="hidden" name="_method" value="delete">';
         $delete .= '<button class="btn py-0 px-1" type="submit"><i class="fas fa-trash-alt"></i></button>';
         $delete .= '</form>';
     } else {
+        $edit = '';
         $delete = '';
     }
 
@@ -35,7 +39,8 @@ foreach ($data['data'] as $item) {
     echo '<div class="card-header">';
     echo '<div class="d-flex justify-content-between">';
     echo '<strong>'.$item['title'].'</strong>';
-    echo '<span class="d-flex align-items-center">by '.$item['user'].'<span class="mx-2">'.$delete.'</span>'.$date.'</span>';
+    echo '<span class="d-flex align-items-center">by '.$item['user'].
+    '<span class="d-flex align-items-center mx-2">'.$edit.$delete.'</span>'.$date.'</span>';
     echo '</div>';
     echo '</div>';
     // pre tags to enable newlines and multiple spaces ... format them later
