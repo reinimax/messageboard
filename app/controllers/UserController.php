83af32a7aa36aae1c809b1eb7b8334651f41326c
reinimax <reinhold.schachner@hotmail.com>
2021-03-12 10:51:59 +0100

    Create UserController

---
+++ app/controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\UserModel;
+use app\lib\Session;
+
+class UserController
+{
+    protected $model;
+    protected $userId;
+    protected $userName;
+
+    public function __construct()
+    {
+        // For each and everything this controller does there needs to be a logged in user.
+        // Therefore I can check for the Login directly each time an object of this class is constructed,
+        // so that I don't need to check separately in each function.
+        if (Session::init()->checkLogin() === false) {
+            header('Location:/logout.php');
+            exit;
+        }
+        $this->model = new UserModel();
+        $this->userId = $_SESSION['user_id'];
+        $this->userName = $_SESSION['user'];
+    }
+
+    /**
+     * Loads the data of the current user
+     * @return array The user data
+     */
+    public function settings()
+    {
+        $data = $this->model->settings($this->userId);
+        return [
+            'title' => 'About me',
+            'content' => 'settings.php',
+            'data' => $data
+        ];
+    }
+}
