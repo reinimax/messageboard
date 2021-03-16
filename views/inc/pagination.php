9ad740af5a6fea3298728f126f2ca7c48ad4a0d0
reinimax <reinhold.schachner@hotmail.com>
2021-03-16 13:02:21 +0100

    Dynamically create pagination URL's so that it works also with the search function

--- views/inc/pagination.php
+++ views/inc/pagination.php
@@ -4,10 +4,27 @@ $postsTotal = count($data['data']);
 $postsPerPage = 5; // hardcoded for now. Later the user may change this number via a select or so
 $pages = ceil($postsTotal/$postsPerPage);
 
-$pagination = '<ul class="pagination justify-content-center">';
-$pagination .= '<li class="page-item"><a class="page-link" href="/index.php?page=1">First</a></li>';
-for ($i = 1; $i <= $pages; $i++) {
-    $pagination .= '<li class="page-item"><a class="page-link" href="/index.php?page='.$i.'">'.$i.'</a></li>';
+$uri = (explode('?', $_SERVER['REQUEST_URI']))[0];
+if ($uri === '/') {
+    $uri = 'index.php';
 }
-$pagination .= '<li class="page-item"><a class="page-link" href="/index.php?page='.$pages.'">Last</a></li>';
+
+if (!empty($_SERVER['QUERY_STRING'])) {
+    // cut out previous page from the query-string
+    $query = (explode('page', $_SERVER['QUERY_STRING']))[0];
+} else {
+    $query = '';
+}
+
+if ($query !== '' && preg_match('/&$/u', $query) == false) {
+    $query .= '&';
+}
+
+
+$pagination = '<ul class="pagination justify-content-center">';
+$pagination .= '<li class="page-item"><a class="page-link" href="'.$uri.'?'.$query.'page=1">First</a></li>';
+for ($i = 1; $i <= $pages; $i++) {
+    $pagination .= '<li class="page-item"><a class="page-link" href="'.$uri.'?'.$query.'page='.$i.'">'.$i.'</a></li>';
+}
+$pagination .= '<li class="page-item"><a class="page-link" href="'.$uri.'?'.$query.'page='.$pages.'">Last</a></li>';
 $pagination .= '</ul>';
