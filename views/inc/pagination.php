3d543605cd53ce0505073748b2b93066b9810346
reinimax <reinhold.schachner@hotmail.com>
2021-03-16 12:17:58 +0100

    Add the markup to a variable $pagination instead of echoing it directly

--- views/inc/pagination.php
+++ views/inc/pagination.php
@@ -4,11 +4,10 @@ $postsTotal = count($data['data']);
 $postsPerPage = 5; // hardcoded for now. Later the user may change this number via a select or so
 $pages = ceil($postsTotal/$postsPerPage);
 
-
-echo '<ul class="pagination justify-content-center">';
-echo '<li class="page-item"><a class="page-link" href="/index.php?page=1">First</a></li>';
+$pagination = '<ul class="pagination justify-content-center">';
+$pagination .= '<li class="page-item"><a class="page-link" href="/index.php?page=1">First</a></li>';
 for ($i = 1; $i <= $pages; $i++) {
-    echo '<li class="page-item"><a class="page-link" href="/index.php?page='.$i.'">'.$i.'</a></li>';
+    $pagination .= '<li class="page-item"><a class="page-link" href="/index.php?page='.$i.'">'.$i.'</a></li>';
 }
-echo '<li class="page-item"><a class="page-link" href="/index.php?page='.$pages.'">Last</a></li>';
-echo '</ul>';
+$pagination .= '<li class="page-item"><a class="page-link" href="/index.php?page='.$pages.'">Last</a></li>';
+$pagination .= '</ul>';
