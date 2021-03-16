7ff07d8a674440ab0f746d31301d8fe26f34f897
reinimax <reinhold.schachner@hotmail.com>
2021-03-16 17:40:15 +0100

    Allow user to set posts per page and fix pagination if $max > $pages

--- views/inc/pagination.php
+++ views/inc/pagination.php
@@ -1,7 +1,7 @@
 <?php
 
 $postsTotal = count($data['data']);
-$postsPerPage = 5; // hardcoded for now. Later the user may change this number via a select or so
+$postsPerPage = (isset($_GET['ppp'])) ? ((int) $_GET['ppp']) : 5;
 $pages = ceil($postsTotal/$postsPerPage);
 
 // set the page
@@ -37,6 +37,9 @@ $disableLast = ($page == $pages) ? 'disabled' : '';
 
 // Set a limit to the number of pagination items
 $max = 5; // this is the maximum number of pagination items
+if ($max > $pages) {
+    $max = $pages;
+}
 $step = floor($max/2); // this is the number of items to the left and to the right of the current item
 $firstDisplay = $page - $step; // the first index to display
 $lastDisplay = $page + $step; // the last index to display
@@ -49,7 +52,7 @@ if ($lastDisplay > $pages) {
     $lastDisplay = $pages;
 }
 
-$pagination = '<ul class="pagination justify-content-center">';
+$pagination = '<div class="d-flex justify-content-center align-items-center"><ul class="pagination justify-content-center">';
 $pagination .= '<li class="page-item '.$disableFirst.'"><a class="page-link" href="'.$uri.'?'.$query.'page=1">First</a></li>';
 for ($i = $firstDisplay; $i <= $lastDisplay; $i++) {
     $active = ($page == $i) ? 'active' : '';
@@ -57,3 +60,22 @@ for ($i = $firstDisplay; $i <= $lastDisplay; $i++) {
 }
 $pagination .= '<li class="page-item '.$disableLast.'"><a class="page-link" href="'.$uri.'?'.$query.'page='.$pages.'">Last</a></li>';
 $pagination .= '</ul>';
+$pagination .= '<form action="'.$uri.'" method="GET" class="form-inline mb-3 ml-2">';
+$pagination .= '<select class="form-control" name="ppp" id="ppp">';
+$values = [5,10,15,20,30]; // the values that can be selected for number of posts per page
+foreach ($values as $val) {
+    $selected = ($postsPerPage == $val) ? 'selected' : '';
+    $pagination .= '<option '.$selected.'>'.$val.'</option>';
+}
+$pagination .= '</select>';
+$pagination .= '<label for="ppp" class="mx-2"> posts per page</label>';
+// add hidden fields to preserve already existing GET-parameters. For now, this is limited to query and limit from searching
+// (page needs not to be preserved as it is calculated anew on fom submission)
+if (isset($_GET['query'])) {
+    $pagination .= '<input type="hidden" name="query" value="'.htmlspecialchars($_GET['query']).'">';
+}
+if (isset($_GET['limit'])) {
+    $pagination .= '<input type="hidden" name="limit" value="'.htmlspecialchars($_GET['limit']).'">';
+}
+$pagination .= '<input class="btn btn-success" type="submit" value="Ok">';
+$pagination .= '</form></div>';
