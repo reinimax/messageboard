ff896bef13dfaa6e817332baa9f2003994354392
reinimax <reinhold.schachner@hotmail.com>
2021-03-20 09:19:14 +0100

    Send correct http header when the 404 not found page is rendered

--- app/core/View.php
+++ app/core/View.php
@@ -6,7 +6,7 @@ class View
 {
     /**
      * Renders a view
-     * @param $data Expects an associative array with keys for title and content,
+     * @param array $data Expects an associative array with keys for title and content,
      * where content is the name of the view that is to be rendered. Data, for example
      * a list of posts from the database, is accessed with the key data.
      */
@@ -16,6 +16,8 @@ class View
         if (!isset($data['content']) || !file_exists(ROOT.'/views/'.$data['content'])) {
             $title = '404 Not found';
             $content = $this->getView('error.php', '');
+            $protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0';
+            header($protocol.' 404 Not Found', true, 404);
         } else {
             $title = $data['title'] ?? 'Messageboard';
             $receivedData = $data['data'] ?? '';
