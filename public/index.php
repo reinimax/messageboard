a250ece3fc09f723e3271790b5b66fbe6b3b5bc4
reinimax <reinhold.schachner@hotmail.com>
2021-03-27 11:33:36 +0100

    Add constants for URL and email

--- public/index.php
+++ public/index.php
@@ -3,6 +3,19 @@
 use app\core\Application;
 
 define('ROOT', dirname(__DIR__));
+
+/**
+ * This constant holds the URL of the site. This is useful because on production or when changing servers, I have
+ * to change the URL only in one place (for example, the URL may be used in Links, in emails ...)
+ */
+define('URL', 'http://messageboard.loc');
+
+/**
+ * Email constants
+ */
+define('EMAIL_ADDR', 'info@messageboard.loc');
+define('EMAIL_NAME', 'reinimax\' Messageboard');
+
 /**
  * Set this variable to false for development in order to enable more detailed error messages,
  * and to true for production to enable more general error messages and disable PHP error reporting
